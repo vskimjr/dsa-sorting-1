398cbff1a5882c67cf3293635ca69d7bfa803af7
Victor Kim <vskimjr@gmail.com>
2024-03-01 15:29:50 -0500

    finished seelctionSort

--- sorting/selection-sort/selectionSort.ts
+++ sorting/selection-sort/selectionSort.ts
@@ -1,7 +1,27 @@
 /** Sort in-place and return sorted array. */
 
 function selectionSort(nums: number[]): number[] {
+
+  if (nums.length === 0) return nums;
+
+  for (let i = 0; i < nums.length; i++){
+    let minimum = nums[i]
+    for (let j = i + 1; j < nums.length; j ++){
+      if (nums[j] < minimum) {
+        let placeholder = nums[i];
+        minimum = nums[j];
+        nums[i] = nums[j];
+        nums[j] = placeholder;
+      }
+    }
+  }
+
   return nums;
 }
 
 export { selectionSort };
+
+// [5, 1, 3, 2, 4]
+// 5
+// .. 1
+// [1,5,3,2,1]
